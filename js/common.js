3c30fa0764cbb227cab774b7b1e3d3d962bb7eee
hejo47 <hejo_47@naver.com>
2022-11-18 17:59:22 +0900

    221118 js 추가

---
+++ js/common.js
@@ -0,0 +1,4 @@
+/* SLIDE */
+const slide1 = document.getElementsByClassName("main_slide1")[0];
+const slide2 = document.getElementsByClassName("main_slide2")[0];
+const slide3 = document.getElementsByClassName("main_slide3")[0];
