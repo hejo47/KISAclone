59916e5153a1c915823a7dc184eca6daef2f547c
hejo47 <hejo_47@naver.com>
2022-11-21 20:29:55 +0900

    221121 작업 내용

--- js/common.js
+++ js/common.js
@@ -2,3 +2,9 @@
 const slide1 = document.getElementsByClassName("main_slide1")[0];
 const slide2 = document.getElementsByClassName("main_slide2")[0];
 const slide3 = document.getElementsByClassName("main_slide3")[0];
+console.log(slide1.style);
+
+
+const slideAction = () => {
+  setInterval(() => document.slide1.style.opacity = 0, 5000);
+}
