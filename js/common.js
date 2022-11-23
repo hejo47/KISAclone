84ade4780918b893401af40d7f5a906ad43614fb
hejo47 <hejo_47@naver.com>
2022-11-23 15:28:49 +0900

    221123 두번째페이지

--- js/common.js
+++ js/common.js
@@ -3,20 +3,20 @@ const slide1 = document.getElementsByClassName("main_slide1")[0];
 const slide2 = document.getElementsByClassName("main_slide2")[0];
 const slide3 = document.getElementsByClassName("main_slide3")[0];
 
-const slideAction = () => {
-  if (slide1.classList.contains("active")) {
-    slide1.classList.remove("active");
-    slide2.classList.add("active");
-  } else if (slide2.classList.contains("active")) {
-    slide2.classList.remove("active");
-    slide3.classList.add("active");
-  } else if (slide3.classList.contains("active")) {
-    slide3.classList.remove("active");
-    slide1.classList.add("active");
-  }
-};
+// const slideAction = () => {
+//   if (slide1.classList.contains("active")) {
+//     slide1.classList.remove("active");
+//     slide2.classList.add("active");
+//   } else if (slide2.classList.contains("active")) {
+//     slide2.classList.remove("active");
+//     slide3.classList.add("active");
+//   } else if (slide3.classList.contains("active")) {
+//     slide3.classList.remove("active");
+//     slide1.classList.add("active");
+//   }
+// };
 
-setInterval(slideAction, 5000);
+// setInterval(slideAction, 5000);
 
 /* HEADER */
 const header = document.getElementsByTagName("header")[0];
@@ -30,6 +30,20 @@ const headerNonActive = () => {
 };
 header.addEventListener("mouseover", headerActive);
 header.addEventListener("mouseout", headerNonActive);
+
+//스크롤이 섹션 3에 있을 때 헤더에 액티브 주기
+// new fullpage("#fullpage", {
+//   onLeave: function (origin, destination, direction, trigger) {
+//     var leavingSection = this;
+//     console.log(this);
+//     //after leaving section 2
+//     if (origin.index == 1 && direction == "down") {
+//       alert("Going to section 3!");
+//     } else if (origin.index == 1 && direction == "up") {
+//       alert("Going to section 1!");
+//     }
+//   },
+// });
 
 /* HEADER - DEPTH */
 // const depth1 = document.querySelectorAll(".depth1");
@@ -69,3 +83,15 @@ $(function () {
   });
   // console.log(1);
 });
+
+// SWIPER
+// Swiper - Pause
+const pauseButton = document.getElementById("swiper-pause-button");
+function swiperPause() {
+  if (mainSlider.autoplay.running == false) {
+    mainSlider.autoplay.start();
+  } else if (mainSlider.autoplay.running == true) {
+    mainSlider.autoplay.stop();
+  }
+}
+pauseButton.addEventListener("click", swiperPause);
